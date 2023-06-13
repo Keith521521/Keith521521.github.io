7edf3135847b54b154abbea240ad935da47a6d2a
Keith521521 <136465489+Keith521521@users.noreply.github.com>
2023-06-14 00:58:02 +0800

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Data {
+    //表示服务是否可以正常的运行
+    public static boolean isRun = true;
+    //表示服务是否可以暂停运行
+    public static boolean isPush = false;
+    //存储服务器可以获取的静态资源的位置
+    public static String resourcePath = "C:/Users/zzwmr/Desktop/Java/课设展示/";
+}
