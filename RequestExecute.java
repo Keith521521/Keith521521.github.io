0c9e6cf07338ad2ad5c6d8913ff65a6c43e576cd
Keith521521 <136465489+Keith521521@users.noreply.github.com>
2023-06-14 19:43:07 +0800

    Add files via upload

--- RequestExecute.java
+++ RequestExecute.java
@@ -1,136 +1,193 @@
 package service;
 
 import data.Data;
-
 import java.io.*;
 import java.net.Socket;
 
-//请求处理线程类
+/**
+ * 请求处理线程类
+ *
+ * @author 张明超
+ *
+ */
 public class RequestExecute extends Thread{
-    //将Socket定义为成员变量，利用构造方法初始化
+    /**
+     * 套接字
+     * 将Socket定义为成员变量，利用构造方法初始化
+     */
     private Socket socket;
+
+    /**
+     * 请求执行
+     *
+     * @param socket 套接字
+     */
     public RequestExecute(Socket socket) {
         this.socket = socket;
     }
+
+    /**
+     * 运行
+     */
     public void run() {
-        //从Socket中取出输入流，然后从输入流中取出数据
-        InputStream in = null;//将字节输入流转换为缓冲字符输入流
-        InputStreamReader reader = null;//转换流
-        BufferedReader bufferedReader = null;//字符缓冲流
-        //声明输出流 输出流是指向客户端的
+        /**
+         * 从Socket中取出输入流，然后从输入流中取出数据
+         */
+
+        /**
+         * 字节输入流in
+         */
+        InputStream in = null;
+        /**
+         *字符输入流reader
+         */
+        InputStreamReader reader = null;
+        /**
+         * 缓冲字符输入流bufferedReader
+         */
+        BufferedReader bufferedReader = null;
+        /**
+         * 字节输出流out
+         */
         OutputStream out = null;
+        /**
+         * 字符输出流pw
+         */
         PrintWriter pw = null;
+
         try{
-            //从Socket中获取字节输出流
+            /**
+             * 将字节输出流包装成字符输出流
+             */
             out = socket.getOutputStream();
-            //将字节输出流包装成字符输出流
             pw = new PrintWriter(out);
-            //先判断服务器是否处于暂停状态
+            /**
+             * 先判断服务器是否处于暂停状态
+             */
             if(Data.isPush) {
-                pw.println("HTTP/1.1 200 OK");//输出响应行
+                pw.println("HTTP/1.1 200 OK");
                 pw.println("Content-Type:text/html;charset=utf-8");
-                //输出空行
-                pw.println();//表示响应头结束，开始响应内容
-                pw.write("<h2>欢迎访问Server1.0</h2>");
-                pw.write("<h2>服务器目前处于暂停状态，请您稍后再试！</h2>");
+                pw.println();
+                pw.write("<body bgcolor=\"#F0F8FF\">");
+                pw.write("<h2 align=\"CENTER\" style=\"font-size: 50px \">抱歉!</h2>");
+                pw.write("<h3 align=\"CENTER\" style=\"font-size: 50px \">服务器目前处于暂停状态，请您稍后再试！</h3>");
+                pw.write("</body>");
                 pw.flush();
                 System.out.println("服务器目前处于暂停状态");
                 return;
             }
-            //从Socket中获取字节输入流
+            /**
+             * 从Socket中获取字节输入流
+             * 把字节输入流包装成字符输入流
+             */
             in = socket.getInputStream();
-            //转换和包装
             reader = new InputStreamReader(in);
+            /**
+             * 把字符输入流转换成缓冲字符输入流
+             */
             bufferedReader = new BufferedReader(reader);
-            //循环的从字符流中获取字符
             String line = null;
             int lineNum = 1;
-            //存储请求路径
+
             String reqPath = "";
             String host = "";
             while ((line = bufferedReader.readLine())!=null) {
                 System.out.println(line);
-                //解析请求行
-                if(lineNum==1) {//第一行  GET /xxx/xx.HTML HTTP1.1
-                    //使用空格分割字符串
+
+                if(lineNum==1) {
+                    /**
+                     * 第一行  GET /xxx/xx.HTML HTTP1.1
+                     * 用空格分割字符串
+                     */
                     String [] arr = line.split(" ");
                     if(arr!=null || arr.length>2) {
-                        reqPath = arr[1];//请求路径
+                        reqPath = arr[1];
                     }else {
                         throw new RuntimeException("请求行解析失败"+line);
                     }
                 }else {
-                    //解析其他行，取出Host的内容
+                    /**
+                     * 解析其他行，取出Host的内容
+                     */
                     String [] arr = line.split(":");
                     if(arr!=null || arr.length==2) {
-                        //取出host
+
                         if(arr[0].equals("Host"))
                             host = arr[1];
                     }
                 }
                 lineNum++;
-                if(line.equals(""))//读取到空行就结束，因为Http请求是长连接，无法读取到文件的末尾
+                /**
+                 * 读取到空行就结束，因为Http请求是长连接，无法读取到文件的末尾
+                 */
+                if(line.equals(""))
                     break;
             }
 
-            //输出请求信息
+            /**
+             * 输出请求信息
+             */
             if(!reqPath.equals("")) {
                 System.out.println("处理请求：http://" + host + reqPath);
-                //根据请求响应客户端 "/“直接响应一个欢迎页面 /…….html 就输出对应的文件内容
-                if(reqPath.equals("/")){//没有资源的名称
-                    pw.println("HTTP/1.1 200 OK");//输出响应行
+                /**
+                 * 没有资源名称
+                 */
+                if(reqPath.equals("/")){
+                    pw.println("HTTP/1.1 200 OK");
                     pw.println("Content-Type:text/html;charset=utf-8");
-                    //输出空行
-                    pw.println();//表示响应头结束，开始响应内容
+
+                    pw.println();
                     pw.write("<body bodystyle=\"text-align:center\"\n" +
-                            "      background=\"img_1.jpg\"\n" +
-                            "      style=\"background-repeat: no-repeat;\n" +
-                            "   background-size: 100%100%;\n" +
-                            "   background-attachment: fixed;\">");
+                            "background=\"zhuomian.png\"\n" +
+                            "style=\"background-repeat: no-repeat;\n" +
+                            "background-size: 100%100%;\n" +
+                            "background-attachment: fixed;\">");
                     pw.write("<br/>");
                     pw.write("<br/>");
                     pw.write("<br/>");
-                    pw.write("<h2 align=\"CENTER\" style=\"font-size: 100px\"> 欢迎访问服务器</h2>");
+                    pw.write("<h2 align=\"CENTER\" style=\"font-size: 120px \"> HELLO!</h2>");
+                    pw.write("<h3 align=\"CENTER\" style=\"font-size: 120px \"> 欢迎访问!</h3>");
                     pw.write("</body>");
                     pw.flush();
                     System.out.println("响应欢迎页面！");
                 }else {
-                    //查找对应的资源   /.html/.jpg/.png/.json……
-                    //取出后缀
+                    /**
+                     * 查找对应的资源   /.html/.jpg/.png/.json……
+                     */
                     String ext = reqPath.substring(reqPath.lastIndexOf(".")+1);
-                    reqPath = reqPath.substring(1);//去除前面的“/”；
-
-
-                    //判断是在根目录下还是在其他的子目录下
-                    if(reqPath.contains("/")) {//子目录
-                        //判断文件是否存在
+                    reqPath = reqPath.substring(1);
+                    /**
+                     * 在子目录下
+                     */
+                    if(reqPath.contains("/")) {
                         File file = new File(Data.resourcePath+reqPath);
                         if(file.exists()&&file.isFile()) {
-                            //将文件内容响应到客户端
                             response200(out,file.getAbsolutePath(),ext);
                         }else {
-                            //响应404页面
                             response404(out);
                         }
-                    }else {//根目录
-                        //判断这个资源是否存在
-                        //获取根目录下的所有的文件的名称
+                    }
+                    /**
+                     * 在根目录下
+                     */
+                    else {
                         File root = new File(Data.resourcePath);
                         if(root.isDirectory()) {
                             File [] list = root.listFiles();
-                            boolean isExist = false;//标记访问的资源是否存在
+                            boolean isExist = false;
                             for(File file : list){
                                 if(file.isFile()&&file.getName().equals(reqPath)) {
-                                    //文件存在
                                     isExist = true;
                                     break;
                                 }
                             }
-                            if(isExist){//文件存在
+                            if(isExist){
                                 response200(out,Data.resourcePath+reqPath,ext);
-                            }else {//文件不存在
+                            }else {
                                 response404(out);
                             }
+                        }else {
+                            throw new RuntimeException("静态资源目录不存在："+Data.resourcePath);
                         }
                     }
                 }
@@ -162,27 +219,30 @@ public class RequestExecute extends Thread{
         }
     }
 
-    //将指定的文件输出到输出流中
+    /**
+     * response200
+     *
+     * @param out      输出流
+     * @param filePath 文件路径
+     * @param ext      文件
+     */
     private void response200(OutputStream out,String filePath,String ext) {
         PrintWriter pw = null;
-        //准备输入流读取磁盘上的文件
         InputStream in = null;
         InputStreamReader reader = null;
         BufferedReader bufferedReader = null;
         try {
             if(ext.equals("jpg")||ext.equals("png")||ext.equals("gif")) {
-                out.write("HTTP/1.1 200 OK\r\n".getBytes());//输出响应行
+                out.write("HTTP/1.1 200 OK\r\n".getBytes());
                 if(ext.equals("jpg"))
                     out.write("Content-Type:image/jpg\r\n".getBytes());
                 else if(ext.equals("png"))
                     out.write("Content-Type:image/png\r\n".getBytes());
                 else if(ext.equals("gif"))
                     out.write("Content-Type:image/gif\r\n".getBytes());
-                //输出一个空行
-                out.write("\r\n".getBytes());//输出空行，表示响应头结束
+                out.write("\r\n".getBytes());
                 System.out.println("响应欢迎页面！");
-                //利用自己而输入流读取文件内容，并且输出到输入流中
-                //创建输入流
+
                 in = new FileInputStream(filePath);
                 int len = -1;
                 byte [] buff = new byte[1024];
@@ -201,13 +261,12 @@ public class RequestExecute extends Thread{
                     pw.println("Content-Type:text/css");
                 else if(ext.equals("json"))
                     pw.println("Content-Type:application/json;charset=utf-8");
-                pw.println();//输出空行，表示响应头结束
+                pw.println();
                 System.out.println("响应欢迎页面！");
-                //初始化输入流
+
                 in = new FileInputStream(filePath);
                 reader = new InputStreamReader(in);
                 bufferedReader = new BufferedReader(reader);
-                //写出数据
                 String line = null;
                 while ((line = bufferedReader.readLine())!=null) {
                     pw.println(line);
@@ -228,21 +287,27 @@ public class RequestExecute extends Thread{
             }
         }
     }
+
+    /**
+     * response404
+     *
+     * @param out 输出流
+     */
     private void response404(OutputStream out) {
         PrintWriter pw = null;
         try {
             pw = new PrintWriter(out);
-            pw.println("HTTP/1.1 404");//输出响应行
+            pw.println("HTTP/1.1 404");
             pw.println("Content-Type:text/html;charset=utf-8");
             //输出空行
-            pw.println();//表示响应头结束，开始响应内容
+            pw.println();
             pw.write("<body bodystyle=\"text-align:center\"\n" +
-                    "      background=\"img_1.jpg\"\n" +
-                    "      style=\"background-repeat: no-repeat;\n" +
+                    "background=\"zhuomian.png\"\n" +
+                    "style=\"background-repeat: no-repeat;\n" +
                     "\tbackground-size: 100%100%;\n" +
                     "\tbackground-attachment: fixed;\">");
             pw.write("<h1 align=\"CENTER\"  style=\"font-size: 100px\">404<h1>");
-            pw.write("<h2 align=\"CENTER\" style=\"font-size: 100px\">客户端所请求的URL在服务端不存在</h2>");
+            pw.write("<h2 align=\"CENTER\" style=\"font-size: 80px\">客户端所请求的URL在服务端不存在</h2>");
             pw.write("</body>");
             pw.flush();
             System.out.println("响应欢迎页面！");
@@ -254,19 +319,9 @@ public class RequestExecute extends Thread{
                 if(pw!=null) {
                     pw.close();
                 }
-            }catch (Exception e) {
-                e.printStackTrace();
+            }catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
     }
 }
-
-
-//200(OK):找到了该资源，并且一切正常
-//304(NOT MODIFIED)该资源在上次请求之后没有任何修改。这通常用于浏览器的缓存机制。
-//401(UNAUTHORIZED)客户端无权访问该资源。这通常会使得浏览器要求用户输入用户名和密码，以登录到服务器。
-//403(FORBIDDEN)客户端未能获得授权。这通常是在401之后输入了不正确的用户名或密码。
-//404(NOT FOUND)在指定的位置不存在所申请的资源
-
-
-
--- Server.java
+++ Server.java
@@ -7,8 +7,17 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server implements Runnable{
-    //监听的端口
+/**
+ * 服务器
+ * @author 张明超
+ * @version 1.0 2023/06/13
+ */
+
+
+public class Server extends Thread{
+    /**
+     * 监听的端口
+     */
     private  int port;
     private MainFrame frame;
     public Server(int port,MainFrame frame) {
@@ -19,30 +28,37 @@ public class Server implements Runnable{
 
     @Override
     public void run() {
-        //声明ServerSocket对象
+        /**
+         * 线程的主程序
+         */
         ServerSocket serverSocket = null;
         try{
             //创建ServerSocket对象，监听本机的8088端口
             serverSocket = new ServerSocket(port);
-            //开始监听
             System.out.println("开始监听……");
-            //输出日志
             frame.printLog("服务开始监听……");
             frame.printLog("监听端口："+port);
-            frame.printLog("静态资源的路径："+Data.resourcePath);
-            while (Data.isRun){//在允许服务器运行的情况下循环监听端口
-                Socket socket = serverSocket.accept(); //等待来自客户端的请求获取和客户端对应的Socket对象
+            frame.printLog("静态资源的路径："+ Data.resourcePath);
+            while (Data.isRun){
+                /**
+                 * 允许服务器运行的情况下循环监听端口
+                 */
+                Socket socket = serverSocket.accept();
                 System.out.println("接收到请求……");
-                //将Socket交给RequestExecute处理
                 RequestExecute re = new RequestExecute(socket);
                 re.start();
             }
-            //运行到这里说明程序要停止
+            /**
+             * 运行到这里说明程序要停止
+             */
             frame.printLog("服务监听停止！！");
         }catch (Exception e) {
             e.printStackTrace();
             throw  new RuntimeException("端口"+port+"监听失败"+e.getMessage());
-        }finally {//关闭ServerSocket
+        }finally {
+            /**
+             * 关闭ServerSocket
+             */
             if(serverSocket!=null) {
                 try {
                     serverSocket.close();
@@ -53,5 +69,3 @@ public class Server implements Runnable{
         }
     }
 }
-
-
